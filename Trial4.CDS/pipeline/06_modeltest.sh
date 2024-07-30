aec9cf87469fa95ebde0eb5ca6aa230136dfb6ff
Tania Kurbessoian, Ph.D <taniakurbessoian@gmail.com>
2024-07-29 21:41:39 -0400

    remove extra folders

--- Trial4.CDS/pipeline/06_modeltest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash -l
-#SBATCH -p general --mem 120gb --out modeltest.log --time 48:00:00
-
-conda activate modeltest-ng
-
-modeltest-ng -d nt -i phyling_align_top50/Top50_multi.mfa -o combined_top50.modeltest -p 24 -q combined_top50.partition
