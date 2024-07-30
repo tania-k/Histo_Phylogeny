b007dea86010950539fda7c1d96f16f5c535c7a2
Tania Kurbessoian, Ph.D <taniakurbessoian@gmail.com>
2024-07-29 21:42:23 -0400

    remove

--- Trial5.AA/pipeline/03_iqtree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes 1 --ntasks 16 --mem 240G --out logs/IQTree.%A.pep.log --time 7-00:00:00
-CPU=24
-module load iqtree/2.2.2.7
-
-IN=phyling_tree/concat_alignments.mfa
-iqtree2 -alrt 1000 -bb 1000 -s $IN -nt AUTO
